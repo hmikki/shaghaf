a2a1232c6d323d89bd51cd6c019d620fbcf65b10
hmikki <30619974+hmikki@users.noreply.github.com>
2021-02-05 19:56:05 +0200

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,21 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('mobile')->unique()->nullable();
             $table->string('email')->unique();
+            $table->string('avatar')->nullable();
+            $table->foreignId('city_id')->nullable();
+            $table->string('iban_number')->unique()->nullable();
+            $table->text('bio')->nullable();
+            $table->tinyInteger('gender')->index()->nullable();
+            $table->string('id_image')->nullable();
+            $table->string('portfolio_image')->nullable();
+            $table->string('device_token')->nullable();
+            $table->string('device_type')->nullable();
+            $table->string('lat')->nullable();
+            $table->string('lng')->nullable();
+            $table->tinyInteger('type')->index();
+            $table->unsignedInteger('provider_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('type');//1:freelancer 2:client
