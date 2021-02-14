98bc4afb98a4ad34aca902205a2259419b80d3b8
hmikki <30619974+hmikki@users.noreply.github.com>
2021-02-14 11:25:26 +0200

    category

--- app/Http/Resources/Api/Product/ProductResource.php
+++ app/Http/Resources/Api/Product/ProductResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Api\Product;
 
 use App\Http\Resources\Api\Home\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
@@ -20,7 +21,7 @@ class ProductResource extends JsonResource
         $Objects['name'] = $this->getName();
         $Objects['description'] = $this->getDescription();
         $Objects['category_id'] = $this->getCategoryId();
-        $Objects['category_name'] = $this->category_id->name;
+        $Objects['category_name'] = Category::where('id', $this->category_id)->first()->name;
         $Objects['sub_category_id'] = $this->getSubCategoryId();
         $Objects['price'] = $this->getPrice();
         $Objects['type'] = $this->getType();
