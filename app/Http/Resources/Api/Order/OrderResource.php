dfe80a0661db451f1ddd10d33de65a5a818f788e
Ahmed M. Elhalaby <eng.ahmed.elhalaby@gmail.com>
2021-02-16 13:02:46 +0200

    update

--- app/Http/Resources/Api/Order/OrderResource.php
+++ app/Http/Resources/Api/Order/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\Order;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\Api\Home\FreelancerResource;
 
@@ -13,14 +14,16 @@ class OrderResource extends JsonResource
         $Objects['id'] = $this->getId();
         $Objects['user_id'] = $this->getUserId();
         $Objects['User'] = new FreelancerResource($this->user);
-        $Objects['Freelancer_id'] = $this->getProviderId();
+        $Objects['Freelancer_id'] = $this->getFreelancerId();
         $Objects['Freelancer'] = new FreelancerResource($this->freelancer);
-        $Objects['price'] = $this->getAmount();
-        $Objects['order_date'] = $this->getOrderDate();
+        $Objects['quantity'] = $this->getQuantity();
+        $Objects['price'] = $this->getPrice();
+        $Objects['total'] = $this->getTotal();
+        $Objects['order_date'] = Carbon::parse($this->created_at);
         $Objects['delivered_date'] = $this->getDeliveredDate();
         $Objects['reject_reason'] = $this->getRejectReason();
         $Objects['cancel_reason'] = $this->getCancelReason();
-        $Objects['rate'] = $this->review()->avg('rate')??0;
+        $Objects['rate'] = $this->reviews()->avg('rate')??0;
         $Objects['status'] = $this->getStatus();
         $Objects['OrderStatuses'] = OrderStatusResource::collection($this->order_statuses);
         return $Objects;
