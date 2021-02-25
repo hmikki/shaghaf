3853bc9a1e02a70adf6ea7a30373219ca9d30a8a
Ahmed M. Elhalaby <eng.ahmed.elhalaby@gmail.com>
2021-02-25 11:06:53 +0200

    update

--- app/Events/CreateMessageEvent.php
+++ app/Events/CreateMessageEvent.php
@@ -11,17 +11,15 @@ class CreateMessageEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
-    public $chat_room_id;
 
-    public function __construct($message,$chat_room_id)
+    public function __construct($message)
     {
         $this->message = $message;
-        $this->chat_room_id = $chat_room_id;
         $this->dontBroadcastToCurrentUser();
     }
 
     public function broadcastOn(): Channel
     {
-        return new Channel('chat_room.'.$this->chat_room_id.'.new_message');
+        return new Channel('chat_room.'.$this->message->chat_room_id.'.new_message');
     }
 }
