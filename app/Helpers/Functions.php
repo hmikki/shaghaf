bf44e5b64dd083701d765be240b157c54215c0d4
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-03-27 11:25:02 +0300

    Update Functions.php

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -333,12 +333,12 @@ class Functions
     }
     public static function CheckPayment($id){
         $url = "https://test.oppwa.com/v1/checkouts/{$id}/payment";
-        $url .= "?entityId=8ac7a4ca785e3293017863cda6690fcc";
+        $url .= "?entityId=8a8294174b7ecb28014b9699220015ca";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Authorization:Bearer OGFjN2E0Yzc3NGE1N2IyYTAxNzRhYmY1ZDZkMDA4NWJ8Rm1CNEpYZFRZSg=='));
+            'Authorization:Bearer OGE4Mjk0MTc0YjdlY2IyODAxNGI5Njk5MjIwMDE1Y2N8c3k2S0pzVDg='));
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// this should be set to true in production
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
